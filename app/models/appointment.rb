3cabd91a89b719dd0f9f832958604a04d0fe3d21
Bateyjosue <josuebatey19@gmail.com>
2023-01-26 17:45:30 +0200

    Add Association and validations

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,2 +1,7 @@
 class Appointment < ApplicationRecord
+  belongs_to :user, class_name: "User", foreign_key: "user_id"
+  belongs_to :technician, class_name: "Technician", foreign_key: "technician_id"
+
+  validates :location, presence: true
+  validates :date, presence: true
 end
--- app/models/technician.rb
+++ app/models/technician.rb
@@ -1,2 +1,9 @@
 class Technician < ApplicationRecord
+  has_many :appointments
+
+  validates :name, presence: true
+  validates :location, presence: true
+  validates :chargers, presence: true
+  validates :specialization, presence: true
+  validates :image, presence: true
 end
