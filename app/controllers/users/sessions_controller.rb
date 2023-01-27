1dc14b8ec4ff67f2cc91fbb995f6fd3b7b5075b4
Kehinde Onifade <onifkay@gmail.com>
2023-01-27 12:06:58 +0100

    update sessions_controller

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,5 +1,5 @@
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  respond_to :json
 
   private
 
@@ -33,36 +33,15 @@ class Users::SessionsController < Devise::SessionsController
 
   def log_out_success
     render json: {
-      message: 'You are logged out',
+      message: 'Signed out successfully',
       status: 204
     }, status: :ok
   end
 
   def log_out_failure
     render json: {
-      message: 'something went wrong'
+      message: 'User have no active session',
+      status: 401
     }, status: :unauthorized
   end
-
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
-
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
-
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
 end
