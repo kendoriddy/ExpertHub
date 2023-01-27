191c2376db127334aa96d634bcba0ec58c6ea2df
Kehinde Onifade <onifkay@gmail.com>
2023-01-27 11:03:54 +0100

    jwt config in devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -11,6 +11,13 @@
 Devise.setup do |config|
   config.jwt do |jwt|
     jwt.secret = ENV['DEVISE_JWT_SECRET_KEY']
+    jwt.dispatch_requests = [
+      ['POST', %r{^/users/sign_in$}]
+    ]
+    jwt.revocation_requests = [
+      ['DELETE', %r{^/users/sign_out}]
+    ]
+    jwt.expiration_time = 1.day.to_i
   end
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
