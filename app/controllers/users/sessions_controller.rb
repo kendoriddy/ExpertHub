017695247dbb0597a68cd9fc5b8fffe0441feece
Kehinde Onifade <onifkay@gmail.com>
2023-01-27 11:45:02 +0100

    sessions and registration controller done

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,7 +1,48 @@
-# frozen_string_literal: true
-
 class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+
+  private
+
+  def respond_with(_resource, _opts = {})
+    self.resource = warden.authenticate!(scope: :user, username: params[:username], password: params[:password])
+    login_success && return if current_user
+
+    login_failed
+  end
+
+  def login_success
+    render json: {
+      message: 'You are logged In',
+      user: current_user,
+      token: request.env['warden-jwt_auth.token'],
+      status: 200
+    }, status: :ok
+  end
+
+  def login_failed
+    render json: {
+      message: 'Invalid username or password',
+      status: 401
+    }, status: :unprocessable_entity
+  end
+
+  def respond_to_on_destroy
+    authenticate_user!
+    log_out_success
+  end
+
+  def log_out_success
+    render json: {
+      message: 'You are logged out',
+      status: 204
+    }, status: :ok
+  end
+
+  def log_out_failure
+    render json: {
+      message: 'something went wrong'
+    }, status: :unauthorized
+  end
 
   # GET /resource/sign_in
   # def new
