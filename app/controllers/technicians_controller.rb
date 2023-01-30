af059f1ab135234e8d2c95811e1a75d84ac8c648
Bateyjosue <josuebatey19@gmail.com>
2023-01-30 10:25:51 +0200

    Add Technicians controller

---
+++ app/controllers/technicians_controller.rb
@@ -0,0 +1,11 @@
+class TechniciansController < ApplicationController
+  def index
+    @technicians = Technician.all
+    render :json => @technicians, each_serializer: TechnicianSerializer
+  end
+
+  def show
+    @technician = Technician.find(params[:id])
+    render json: @technician, each_serializer: TechnicianSerializer
+  end
+end
