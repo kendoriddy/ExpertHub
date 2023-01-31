c47861d5ad800f3f574877aeeca266ce7e8eb41a
Kehinde Onifade <onifkay@gmail.com>
2023-01-31 08:48:01 +0100

    Fix linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-          :jwt_authenticatable, jwt_revocation_strategy: self
+         :jwt_authenticatable, jwt_revocation_strategy: self
 
   validates :name, presence: true, uniqueness: true
 
