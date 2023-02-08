fc2f95a2d914bbe10415352d3f0e524f5941f331
Kehinde Onifade <onifkay@gmail.com>
2023-02-08 10:22:27 +0100

    update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Rails.application.configure do
+    # Your existing configuration options
+    # ...
+  
+    # Add this line to configure your mailer
+    config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+end
+  
